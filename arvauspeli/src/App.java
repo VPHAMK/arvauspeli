51f7ade6851f85968642d1412bef469e3ff96d6a
VPHAMK <amk1004741@student.hamk.fi>
2024-09-06 13:29:58 +0300

    Lisätty while looppi ja arvaukset

--- arvauspeli/src/App.java
+++ arvauspeli/src/App.java
@@ -5,19 +5,28 @@ public class App {
         
         String vastaus = "Emma";
         String arvaus;
+        int arvaukset = 1;
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Arvaa nimi");
-        arvaus = in.nextLine();
-
-        if(arvaus.equals(arvaus))
+        do
         {
-            System.out.println("Voitit");
-        }
+            System.out.println("Arvaa nimi");
+            arvaus = in.nextLine();
 
-        else
-        {
-            System.out.println("hävisit");
+            if(arvaus.equals(vastaus))
+            {
+                System.out.println("Oikein");
+                System.out.println("Arvasit " + arvaukset + " kertaa.");
+            }
+            
+            else
+            {
+                arvaukset++;
+                System.out.println("hävisit");
+            }
+            
         }
+        while(!arvaus.equals(vastaus));        
+        
     }
 }
